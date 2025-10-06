55f60fb40210b87f16c745c4281de103a5d2a1d7
prasad2206 <60003248+prasad2206@users.noreply.github.com>
2025-10-06 17:39:43 +0530

    Configured Entity Framework Core and connected SQL Server

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configure Entity Framework Core with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
